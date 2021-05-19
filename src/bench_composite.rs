d31a981b59ebcb3b491714a850d2c40c63cc513c
Georgi Zlatarev <45011053+ghzlatarev@users.noreply.github.com>
2021-05-19 18:47:27 +0300

    [Pallet-Manta-Pay] #37 - Configure weights in the manta pay pallet (#38)

--- src/bench_composite.rs
+++ src/bench_composite.rs
@@ -75,6 +75,7 @@ impl frame_system::Config for Test {
 
 impl Config for Test {
 	type Event = ();
+	type WeightInfo = ();
 }
 
 pub struct ExtBuilder {}
--- src/test/frame_hardcode.rs
+++ src/test/frame_hardcode.rs
@@ -73,6 +73,7 @@ impl frame_system::Config for Test {
 
 impl Config for Test {
 	type Event = ();
+    type WeightInfo = ();
 }
 type Assets = Module<Test>;
 
