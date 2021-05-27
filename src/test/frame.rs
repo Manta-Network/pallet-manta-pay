060165c4711b2754fcc90bc39219b768d962a4b4
GHZ <ghzlatarev@gmail.com>
2021-05-27 14:30:27 +0300

    Added a couple of Reclaim tests. Restructured the file, added comments and reused code

--- src/test/frame.rs
+++ src/test/frame.rs
@@ -90,6 +90,8 @@ fn new_test_ext() -> sp_io::TestExternalities {
 
 // todo: write must-fail tests for cross-asset-id tests
 
+// Misc tests:
+
 #[test]
 fn test_constants_should_work() {
 	new_test_ext().execute_with(|| {
@@ -104,6 +106,50 @@ fn test_constants_should_work() {
 		assert_eq!(commit_param_checksum, commit_param_checksum_local);
 	});
 }
+
+#[test]
+fn issuing_asset_units_to_issuer_should_work() {
+	new_test_ext().execute_with(|| {
+		initialize_test(100);
+	});
+}
+
+#[test]
+fn querying_total_supply_should_work() {
+	new_test_ext().execute_with(|| {
+		initialize_test(100);
+		assert_ok!(Assets::transfer_asset(Origin::signed(1), 2, TEST_ASSET, 50));
+		assert_eq!(Assets::balance(1, TEST_ASSET), 50);
+		assert_eq!(Assets::balance(2, TEST_ASSET), 50);
+		assert_ok!(Assets::transfer_asset(Origin::signed(2), 3, TEST_ASSET, 31));
+		assert_eq!(Assets::balance(1, TEST_ASSET), 50);
+		assert_eq!(Assets::balance(2, TEST_ASSET), 19);
+		assert_eq!(Assets::balance(3, TEST_ASSET), 31);
+		assert_eq!(Assets::total_supply(TEST_ASSET), 100);
+	});
+}
+
+#[test]
+fn destroying_asset_balance_with_positive_balance_should_work() {
+	new_test_ext().execute_with(|| {
+		initialize_test(100);
+	});
+}
+
+// Init tests:
+
+#[test]
+fn cannot_init_twice() {
+	new_test_ext().execute_with(|| {
+		assert_ok!(Assets::init_asset(Origin::signed(1), TEST_ASSET, 100));
+		assert_noop!(
+			Assets::init_asset(Origin::signed(1), TEST_ASSET, 100),
+			Error::<Test>::AlreadyInitialized
+		);
+	});
+}
+
+// Mint tests:
 
 #[test]
 fn test_mint_should_work() {
@@ -245,6 +291,8 @@ fn mint_with_commit_param_mismatch_should_not_work() {
 	});
 }
 
+// Transfer tests:
+
 #[test]
 fn test_transfer_should_work() {
 	new_test_ext().execute_with(|| transfer_test_helper(1));
@@ -254,39 +302,6 @@ fn test_transfer_should_work() {
 #[test]
 fn test_transfer_should_work_super_long() {
 	new_test_ext().execute_with(|| transfer_test_helper(400));
-}
-
-#[test]
-fn test_reclaim_should_work() {
-	new_test_ext().execute_with(|| reclaim_test_helper(1));
-}
-
-#[ignore]
-#[test]
-fn test_reclaim_should_work_super_long() {
-	new_test_ext().execute_with(|| reclaim_test_helper(400));
-}
-
-#[test]
-fn issuing_asset_units_to_issuer_should_work() {
-	new_test_ext().execute_with(|| {
-		initialize_test(100);
-	});
-}
-
-#[test]
-fn querying_total_supply_should_work() {
-	new_test_ext().execute_with(|| {
-		initialize_test(100);
-		assert_ok!(Assets::transfer_asset(Origin::signed(1), 2, TEST_ASSET, 50));
-		assert_eq!(Assets::balance(1, TEST_ASSET), 50);
-		assert_eq!(Assets::balance(2, TEST_ASSET), 50);
-		assert_ok!(Assets::transfer_asset(Origin::signed(2), 3, TEST_ASSET, 31));
-		assert_eq!(Assets::balance(1, TEST_ASSET), 50);
-		assert_eq!(Assets::balance(2, TEST_ASSET), 19);
-		assert_eq!(Assets::balance(3, TEST_ASSET), 31);
-		assert_eq!(Assets::total_supply(TEST_ASSET), 100);
-	});
 }
 
 #[test]
@@ -361,20 +376,6 @@ fn transferring_without_init_should_not_work() {
 	});
 }
 
-fn load_zkp_keys(file_name: &str) -> Groth16Pk {
-	let mut file = File::open(file_name).unwrap();
-	let mut transfer_key_bytes: Vec<u8> = vec![];
-	file.read_to_end(&mut transfer_key_bytes).unwrap();
-	let buf: &[u8] = transfer_key_bytes.as_ref();
-	Groth16Pk::deserialize_unchecked(buf).unwrap()
-}
-
-fn initialize_test(amount: u64) {
-	assert_ok!(Assets::init_asset(Origin::signed(1), TEST_ASSET, amount));
-	assert_eq!(Assets::balance(1, TEST_ASSET), amount);
-	assert_eq!(PoolBalance::get(TEST_ASSET), 0);
-}
-
 #[test]
 fn transferring_spent_coin_should_not_work_sender_1() {
 	new_test_ext().execute_with(|| {
@@ -429,44 +430,6 @@ fn transferring_spent_coin_should_not_work_sender_1() {
 			);
 		}
 	});
-}
-
-fn prepare_private_transfer_payload(
-	senders: &Vec<MantaAsset>,
-	commit_param: &CommitmentParam,
-	hash_param: &HashParam,
-	pk: &Groth16Pk,
-	receivers_processed: &Vec<MantaAssetProcessedReceiver>,
-	rng: &mut ChaCha20Rng,
-	idx: usize,
-) -> [u8; PRIVATE_TRANSFER_PAYLOAD_SIZE] {
-	let coin_shards = CoinShards::get();
-
-	// build sender meta data
-	let sender_1 = senders[idx * 2].clone();
-	let sender_2 = senders[idx * 2 + 1].clone();
-	let shard_index_1 = sender_1.commitment[0] as usize;
-	let shard_index_2 = sender_2.commitment[0] as usize;
-	let list_1 = coin_shards.shard[shard_index_1].list.clone();
-	let sender_1 = SenderMetaData::build(hash_param.clone(), sender_1, &list_1);
-	let list_2 = coin_shards.shard[shard_index_2].list.clone();
-	let sender_2 = SenderMetaData::build(hash_param.clone(), sender_2, &list_2);
-
-	// extract the receivers
-	let receiver_1 = receivers_processed[idx * 2 + 1].clone();
-	let receiver_2 = receivers_processed[idx * 2].clone();
-
-	// form the transaction payload
-	generate_private_transfer_payload(
-		commit_param.clone(),
-		hash_param.clone(),
-		&pk,
-		sender_1,
-		sender_2,
-		receiver_1,
-		receiver_2,
-		rng,
-	)
 }
 
 #[test]
@@ -817,23 +780,88 @@ fn transferring_with_zkp_verification_fail_should_not_work() {
 	});
 }
 
+// Reclaim tests:
+
 #[test]
-fn destroying_asset_balance_with_positive_balance_should_work() {
+fn test_reclaim_should_work() {
+	new_test_ext().execute_with(|| reclaim_test_helper(1));
+}
+
+#[ignore]
+#[test]
+fn test_reclaim_should_work_super_long() {
+	new_test_ext().execute_with(|| reclaim_test_helper(400));
+}
+
+#[test]
+fn reclaim_without_init_should_not_work() {
 	new_test_ext().execute_with(|| {
-		initialize_test(100);
+		let payload = [0u8; RECLAIM_PAYLOAD_SIZE];
+
+		assert_noop!(
+			Assets::reclaim(Origin::signed(1), payload),
+			Error::<Test>::BasecoinNotInit
+		);
 	});
 }
 
 #[test]
-fn cannot_init_twice() {
+fn reclaim_with_hash_param_mismatch_should_not_work() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(Assets::init_asset(Origin::signed(1), TEST_ASSET, 100));
+		initialize_test(10_000_000);
+
+		let payload = [0u8; RECLAIM_PAYLOAD_SIZE];
+		HashParamChecksum::put([3u8; 32]);
+
+		// invoke the transfer event
 		assert_noop!(
-			Assets::init_asset(Origin::signed(1), TEST_ASSET, 100),
-			Error::<Test>::AlreadyInitialized
+			Assets::reclaim(Origin::signed(1), payload),
+			Error::<Test>::MintFail
 		);
 	});
 }
+
+#[test]
+fn reclaim_spent_coin_should_not_work_sender_1() {
+	new_test_ext().execute_with(|| {
+		initialize_test(10_000_000);
+
+		let hash_param = HashParam::deserialize(HASH_PARAM.data);
+		let commit_param = CommitmentParam::deserialize(COMMIT_PARAM.data);
+
+		let iter = 1;
+		let size = iter << 1;
+		let senders = mint_tokens_helper(size);
+
+		let mut rng = ChaCha20Rng::from_seed([3u8; 32]);
+		let mut sk = [0u8; 32];
+
+		let pk = load_zkp_keys("reclaim_pk.bin");
+		let vk_checksum = ReclaimZKPKeyChecksum::get();
+		assert_eq!(RECLAIM_PK.get_checksum(), vk_checksum);
+
+		for i in 0usize..iter {
+			let (payload, _, _, _) = prepare_reclaim_payload(
+				&senders,
+				&commit_param,
+				&hash_param,
+				&pk,
+				&mut sk,
+				&mut rng,
+				i,
+			);
+
+			assert_ok!(Assets::reclaim(Origin::signed(1), payload));
+
+			assert_noop!(
+				Assets::reclaim(Origin::signed(1), payload),
+				Error::<Test>::PoolOverdrawn
+			);
+		}
+	});
+}
+
+// Helper functions:
 
 fn mint_tokens_helper(size: usize) -> Vec<MantaAsset> {
 	let commit_param = CommitmentParam::deserialize(COMMIT_PARAM.data);
@@ -979,36 +1007,14 @@ fn reclaim_test_helper(iter: usize) {
 	assert_eq!(RECLAIM_PK.get_checksum(), vk_checksum);
 
 	for i in 0usize..iter {
-		let coin_shards = CoinShards::get();
-
-		// build sender mata data
-		let sender_1 = senders[i * 2].clone();
-		let sender_2 = senders[i * 2 + 1].clone();
-		let shard_index_1 = sender_1.commitment[0] as usize;
-		let shard_index_2 = sender_2.commitment[0] as usize;
-		let list_1 = coin_shards.shard[shard_index_1].list.clone();
-		let sender_1 = SenderMetaData::build(hash_param.clone(), sender_1, &list_1);
-		let list_2 = coin_shards.shard[shard_index_2].list.clone();
-		let sender_2 = SenderMetaData::build(hash_param.clone(), sender_2, &list_2);
-
-		rng.fill_bytes(&mut sk);
-		let receiver_full =
-			MantaAssetFullReceiver::sample(&commit_param, &sk, &TEST_ASSET, &(), &mut rng);
-		let receiver = receiver_full.prepared.process(&10, &mut rng);
-
-		let reclaim_value =
-			sender_1.asset.priv_info.value + sender_2.asset.priv_info.value - receiver.value;
-
-		// form the transaction payload
-		let payload = generate_reclaim_payload(
-			commit_param.clone(),
-			hash_param.clone(),
+		let (payload, sender_1, sender_2, reclaim_value) = prepare_reclaim_payload(
+			&senders,
+			&commit_param,
+			&hash_param,
 			&pk,
-			sender_1.clone(),
-			sender_2.clone(),
-			receiver,
-			reclaim_value,
+			&mut sk,
 			&mut rng,
+			i,
 		);
 
 		// invoke the reclaim event
@@ -1026,4 +1032,105 @@ fn reclaim_test_helper(iter: usize) {
 	}
 	let enc_value_list = EncValueList::get();
 	assert_eq!(enc_value_list.len(), iter);
+}
+
+fn prepare_private_transfer_payload(
+	senders: &Vec<MantaAsset>,
+	commit_param: &CommitmentParam,
+	hash_param: &HashParam,
+	pk: &Groth16Pk,
+	receivers_processed: &Vec<MantaAssetProcessedReceiver>,
+	rng: &mut ChaCha20Rng,
+	idx: usize,
+) -> [u8; PRIVATE_TRANSFER_PAYLOAD_SIZE] {
+	// build sender mata data
+	let (sender_1, sender_2) = build_sender_meta_data(&senders, &hash_param, idx);
+
+	// extract the receivers
+	let receiver_1 = receivers_processed[idx * 2 + 1].clone();
+	let receiver_2 = receivers_processed[idx * 2].clone();
+
+	// form the transaction payload
+	generate_private_transfer_payload(
+		commit_param.clone(),
+		hash_param.clone(),
+		&pk,
+		sender_1,
+		sender_2,
+		receiver_1,
+		receiver_2,
+		rng,
+	)
+}
+
+fn prepare_reclaim_payload(
+	senders: &Vec<MantaAsset>,
+	commit_param: &CommitmentParam,
+	hash_param: &HashParam,
+	pk: &Groth16Pk,
+	sk: &mut [u8; 32],
+	rng: &mut ChaCha20Rng,
+	idx: usize,
+) -> (
+	[u8; RECLAIM_PAYLOAD_SIZE],
+	zkp::SenderMetaData,
+	zkp::SenderMetaData,
+	u64,
+) {
+	// build sender mata data
+	let (sender_1, sender_2) = build_sender_meta_data(&senders, &hash_param, idx);
+
+	rng.fill_bytes(&mut sk[..]);
+	let receiver_full = MantaAssetFullReceiver::sample(&commit_param, &sk, &TEST_ASSET, &(), rng);
+	let receiver = receiver_full.prepared.process(&10, rng);
+
+	let reclaim_value =
+		sender_1.asset.priv_info.value + sender_2.asset.priv_info.value - receiver.value;
+
+	// form the transaction payload
+	let payload = generate_reclaim_payload(
+		commit_param.clone(),
+		hash_param.clone(),
+		&pk,
+		sender_1.clone(),
+		sender_2.clone(),
+		receiver,
+		reclaim_value,
+		rng,
+	);
+
+	(payload, sender_1, sender_2, reclaim_value)
+}
+
+fn load_zkp_keys(file_name: &str) -> Groth16Pk {
+	let mut file = File::open(file_name).unwrap();
+	let mut transfer_key_bytes: Vec<u8> = vec![];
+	file.read_to_end(&mut transfer_key_bytes).unwrap();
+	let buf: &[u8] = transfer_key_bytes.as_ref();
+	Groth16Pk::deserialize_unchecked(buf).unwrap()
+}
+
+fn initialize_test(amount: u64) {
+	assert_ok!(Assets::init_asset(Origin::signed(1), TEST_ASSET, amount));
+	assert_eq!(Assets::balance(1, TEST_ASSET), amount);
+	assert_eq!(PoolBalance::get(TEST_ASSET), 0);
+}
+
+fn build_sender_meta_data(
+	senders: &Vec<MantaAsset>,
+	hash_param: &HashParam,
+	idx: usize,
+) -> (zkp::SenderMetaData, zkp::SenderMetaData) {
+	let coin_shards = CoinShards::get();
+
+	let sender_1 = senders[idx * 2].clone();
+	let sender_2 = senders[idx * 2 + 1].clone();
+	let shard_index_1 = sender_1.commitment[0] as usize;
+	let shard_index_2 = sender_2.commitment[0] as usize;
+	let list_1 = coin_shards.shard[shard_index_1].list.clone();
+	let out_sender_1 = SenderMetaData::build(hash_param.clone(), sender_1, &list_1).into();
+	let list_2 = coin_shards.shard[shard_index_2].list.clone();
+	let out_sender_2 = SenderMetaData::build(hash_param.clone(), sender_2, &list_2);
+
+	(out_sender_1, out_sender_2)
 }
