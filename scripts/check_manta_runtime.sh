da52f69d5a9ba4018f79352d890edc5c4908bc70
GHZ <ghzlatarev@gmail.com>
2021-06-02 17:47:11 +0300

    Test

--- scripts/check_manta_runtime.sh
+++ scripts/check_manta_runtime.sh
@@ -4,6 +4,8 @@ git clone https://github.com/Manta-Network/Manta.git
 
 cd Manta/
 
+pwd
+
 sed -i "s@pallet-manta-pay = { git='https://github.com/Manta-Network/pallet-manta-pay', branch='calamari', default-features = false }@pallet-manta-pay = {path= '../../../../', default-features = false }@g" ./runtimes/manta/runtime/Cargo.toml
 
 cat Cargo.toml
