30dc7fbae9c85f6f9d54a96d4c9436c950e23c86
GHZ <ghzlatarev@gmail.com>
2021-06-02 18:09:46 +0300

    Add an extra space so string in script matches to string in manta runtime cargo.toml file

--- scripts/check_manta_runtime.sh
+++ scripts/check_manta_runtime.sh
@@ -4,10 +4,6 @@ git clone https://github.com/Manta-Network/Manta.git
 
 cd Manta/
 
-pwd
-
-sed -i "s@pallet-manta-pay = {git='https://github.com/Manta-Network/pallet-manta-pay', branch='calamari', default-features = false }@pallet-manta-pay = {path= '../../../../', default-features = false }@g" ./runtimes/manta/runtime/Cargo.toml
-
-cat Cargo.toml
+sed -i "s@pallet-manta-pay = { git='https://github.com/Manta-Network/pallet-manta-pay', branch='calamari', default-features = false }@pallet-manta-pay = {path= '../../../../', default-features = false }@g" ./runtimes/manta/runtime/Cargo.toml
          
-cargo build --release --all-features
+cargo build --all-features
