dd9cd7be18d802dcf169d3c6d27ae6dbdb26adc0
GHZ <ghzlatarev@gmail.com>
2021-06-22 11:37:39 +0300

    Update ci and integration checks to include no-default feature builds and wasm benchmarking respectively

--- scripts/check_manta_runtime.sh
+++ scripts/check_manta_runtime.sh
@@ -9,4 +9,12 @@ cd Manta/
 sed -i "s@pallet-manta-pay = { git='https://github.com/Manta-Network/pallet-manta-pay', branch='calamari', default-features = false }@pallet-manta-pay = {path= '../../../../', default-features = false }@g" ./runtimes/manta/runtime/Cargo.toml
          
 cargo build
+cargo build --no-default-features
 cargo build --all-features
+
+# Check Wasm benchmarking 
+target/debug/manta benchmark --pallet pallet_manta_pay --extrinsic init_asset --execution=Wasm --wasm-execution Compiled --repeat 100 
+target/debug/manta benchmark --pallet pallet_manta_pay --extrinsic transfer_asset --execution=Wasm --wasm-execution Compiled --repeat 100
+target/debug/manta benchmark --pallet pallet_manta_pay --extrinsic mint_private_asset --execution=Wasm --wasm-execution Compiled --repeat 10
+target/debug/manta benchmark --pallet pallet_manta_pay --extrinsic private_transfer --execution=Wasm --wasm-execution Compiled --repeat 10
+target/debug/manta benchmark --pallet pallet_manta_pay --extrinsic reclaim --execution=Wasm --wasm-execution Compiled --repeat 10 
