8643b7c18de90e9beb30ab5da5ef0b8f32053cb8
Brandon H. Gomes <bhgomes@pm.me>
2022-02-07 01:00:45 -0500

    chore(fmt): fix formatting

--- src/benchmark/mod.rs
+++ src/benchmark/mod.rs
@@ -15,10 +15,10 @@
 // along with pallet-manta-pay.  If not, see <http://www.gnu.org/licenses/>.
 
 use crate::{
-	benchmark::precomputed_coins::{
-		MINT, PRIVATE_TRANSFER, PRIVATE_TRANSFER_INPUT, RECLAIM, RECLAIM_INPUT,
-	},
-	Asset, Balances, Call, Config, Event, Pallet, TransferPost,
+    benchmark::precomputed_coins::{
+        MINT, PRIVATE_TRANSFER, PRIVATE_TRANSFER_INPUT, RECLAIM, RECLAIM_INPUT,
+    },
+    Asset, Balances, Call, Config, Event, Pallet, TransferPost,
 };
 use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
@@ -31,73 +31,73 @@ mod precomputed_coins;
 #[inline]
 pub fn assert_last_event<T, E>(event: E)
 where
-	T: Config,
-	E: Into<<T as Config>::Event>,
+    T: Config,
+    E: Into<<T as Config>::Event>,
 {
-	let events = frame_system::Pallet::<T>::events();
-	assert_eq!(events[events.len() - 1].event, event.into().into());
+    let events = frame_system::Pallet::<T>::events();
+    assert_eq!(events[events.len() - 1].event, event.into().into());
 }
 
 benchmarks! {
-	transfer {
-		let caller: T::AccountId = whitelisted_caller();
-		let origin = T::Origin::from(RawOrigin::Signed(caller.clone()));
-		Pallet::<T>::init_asset(&caller, 0, 1_000);
-		let recipient: T::AccountId = account("recipient", 0, 0);
-		let recipient_lookup = T::Lookup::unlookup(recipient.clone());
-		let asset = Asset::new(0, 10);
-	}: transfer (
-		RawOrigin::Signed(caller.clone()),
-		recipient_lookup,
-		asset
-	) verify {
-		assert_last_event::<T, _>(Event::Transfer { asset, source: caller, sink: recipient.clone() });
-		assert_eq!(Balances::<T>::get(recipient, asset.id), asset.value);
-	}
+    transfer {
+        let caller: T::AccountId = whitelisted_caller();
+        let origin = T::Origin::from(RawOrigin::Signed(caller.clone()));
+        Pallet::<T>::init_asset(&caller, 0, 1_000);
+        let recipient: T::AccountId = account("recipient", 0, 0);
+        let recipient_lookup = T::Lookup::unlookup(recipient.clone());
+        let asset = Asset::new(0, 10);
+    }: transfer (
+        RawOrigin::Signed(caller.clone()),
+        recipient_lookup,
+        asset
+    ) verify {
+        assert_last_event::<T, _>(Event::Transfer { asset, source: caller, sink: recipient.clone() });
+        assert_eq!(Balances::<T>::get(recipient, asset.id), asset.value);
+    }
 
-	mint {
-		let caller: T::AccountId = whitelisted_caller();
-		let origin = T::Origin::from(RawOrigin::Signed(caller.clone()));
-		Pallet::<T>::init_asset(&caller, 0, 1_000_000);
-		let mint_post = TransferPost::decode(&mut &*MINT).unwrap();
-		let asset = Asset::new(mint_post.asset_id.unwrap(), mint_post.sources[0]);
-	}: mint (
-		RawOrigin::Signed(caller.clone()),
-		mint_post
-	) verify {
-		assert_last_event::<T, _>(Event::Mint { asset, source: caller.clone() });
-		assert_eq!(Balances::<T>::get(caller, asset.id), 1_000_000 - asset.value);
-	}
+    mint {
+        let caller: T::AccountId = whitelisted_caller();
+        let origin = T::Origin::from(RawOrigin::Signed(caller.clone()));
+        Pallet::<T>::init_asset(&caller, 0, 1_000_000);
+        let mint_post = TransferPost::decode(&mut &*MINT).unwrap();
+        let asset = Asset::new(mint_post.asset_id.unwrap(), mint_post.sources[0]);
+    }: mint (
+        RawOrigin::Signed(caller.clone()),
+        mint_post
+    ) verify {
+        assert_last_event::<T, _>(Event::Mint { asset, source: caller.clone() });
+        assert_eq!(Balances::<T>::get(caller, asset.id), 1_000_000 - asset.value);
+    }
 
-	private_transfer {
-		let caller: T::AccountId = whitelisted_caller();
-		let origin = T::Origin::from(RawOrigin::Signed(caller.clone()));
-		Pallet::<T>::init_asset(&caller, 0, 1_000_000);
-		for coin in PRIVATE_TRANSFER_INPUT {
-			Pallet::<T>::mint(origin.clone(), TransferPost::decode(&mut &**coin).unwrap()).unwrap();
-		}
-		let private_transfer_post = TransferPost::decode(&mut &*PRIVATE_TRANSFER).unwrap();
-	}: private_transfer (
-		RawOrigin::Signed(caller.clone()),
-		private_transfer_post
-	) verify {
-		assert_last_event::<T, _>(Event::PrivateTransfer { origin: caller });
-	}
+    private_transfer {
+        let caller: T::AccountId = whitelisted_caller();
+        let origin = T::Origin::from(RawOrigin::Signed(caller.clone()));
+        Pallet::<T>::init_asset(&caller, 0, 1_000_000);
+        for coin in PRIVATE_TRANSFER_INPUT {
+            Pallet::<T>::mint(origin.clone(), TransferPost::decode(&mut &**coin).unwrap()).unwrap();
+        }
+        let private_transfer_post = TransferPost::decode(&mut &*PRIVATE_TRANSFER).unwrap();
+    }: private_transfer (
+        RawOrigin::Signed(caller.clone()),
+        private_transfer_post
+    ) verify {
+        assert_last_event::<T, _>(Event::PrivateTransfer { origin: caller });
+    }
 
-	reclaim {
-		let caller: T::AccountId = whitelisted_caller();
-		let origin = T::Origin::from(RawOrigin::Signed(caller.clone()));
-		Pallet::<T>::init_asset(&caller, 0, 1_000_000);
-		for coin in RECLAIM_INPUT {
-			Pallet::<T>::mint(origin.clone(), TransferPost::decode(&mut &**coin).unwrap()).unwrap();
-		}
-		let reclaim_post = TransferPost::decode(&mut &*RECLAIM).unwrap();
-	}: reclaim (
-		RawOrigin::Signed(caller.clone()),
-		reclaim_post
-	) verify {
-		assert_last_event::<T, _>(Event::Reclaim { asset: Asset::new(0, 10_000), sink: caller });
-	}
+    reclaim {
+        let caller: T::AccountId = whitelisted_caller();
+        let origin = T::Origin::from(RawOrigin::Signed(caller.clone()));
+        Pallet::<T>::init_asset(&caller, 0, 1_000_000);
+        for coin in RECLAIM_INPUT {
+            Pallet::<T>::mint(origin.clone(), TransferPost::decode(&mut &**coin).unwrap()).unwrap();
+        }
+        let reclaim_post = TransferPost::decode(&mut &*RECLAIM).unwrap();
+    }: reclaim (
+        RawOrigin::Signed(caller.clone()),
+        reclaim_post
+    ) verify {
+        assert_last_event::<T, _>(Event::Reclaim { asset: Asset::new(0, 10_000), sink: caller });
+    }
 }
 
 impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);
